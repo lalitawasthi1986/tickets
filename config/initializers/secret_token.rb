328e5370ce5cc143ecabda8500f51c5b5cba6415
lalit awasthi <lalit.redfuze@gmail.com>
2012-12-26 16:26:22 +0530

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = 'd1b7b9493befc5f135764f8e04fd5dce4bf829ea35a8f522d746b6795ab2566c49b5ff1ae0aa45836f6e253027a8da9cce759d33b27a3ca2883314d2f84498b2'
